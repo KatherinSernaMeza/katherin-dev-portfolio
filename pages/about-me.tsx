d0985966c1ea2e9a280b43e6f0f8a14b3aaa7947
Katherin Serna <katherin.serna.m@gmail.com>
2024-03-15 10:17:48 -0700

    #Comment: Set up Routing

---
+++ pages/about-me.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const aboutme = (props: Props) => {
+  return <div>about-me</div>;
+};
+
+export default aboutme;
---
+++ pages/skills.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const skills = (props: Props) => {
+  return <div>skills</div>;
+};
+
+export default skills;
